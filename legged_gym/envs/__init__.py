439a82f1a8ee6ea104d9926d25970989328d8a4d
yn_200 <2809805417@qq.com>
2025-06-17 16:46:54 +1000

    feat:go2_stand

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -1,6 +1,7 @@
 from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
 
-from legged_gym.envs.go2.go2_config import GO2RoughCfg, GO2RoughCfgPPO
+from legged_gym.envs.go2.go2_config import GO2CfgPPO_Handstand, GO2Cfg_Handstand
+from legged_gym.envs.go2.go2_env import Go2_stand
 from legged_gym.envs.h1.h1_config import H1RoughCfg, H1RoughCfgPPO
 from legged_gym.envs.h1.h1_env import H1Robot
 from legged_gym.envs.h1_2.h1_2_config import H1_2RoughCfg, H1_2RoughCfgPPO
@@ -9,9 +10,10 @@ from legged_gym.envs.g1.g1_config import G1RoughCfg, G1RoughCfgPPO
 from legged_gym.envs.g1.g1_env import G1Robot
 from .base.legged_robot import LeggedRobot
 
+
 from legged_gym.utils.task_registry import task_registry
 
-task_registry.register( "go2", LeggedRobot, GO2RoughCfg(), GO2RoughCfgPPO())
+task_registry.register( "go2", Go2_stand, GO2Cfg_Handstand(), GO2CfgPPO_Handstand())
 task_registry.register( "h1", H1Robot, H1RoughCfg(), H1RoughCfgPPO())
 task_registry.register( "h1_2", H1_2Robot, H1_2RoughCfg(), H1_2RoughCfgPPO())
 task_registry.register( "g1", G1Robot, G1RoughCfg(), G1RoughCfgPPO())
